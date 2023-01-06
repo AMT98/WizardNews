dfdacd58784146837401692b2f5b1df544613263
AMT98 <aswinmalla12@gmail.com>
2023-01-05 20:26:41 -0700

    Add new directory

---
+++ wnews/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+app.use(morgan( 'dev' ));  
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const PORT = 1337;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
+
