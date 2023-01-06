21a346d5e11b0868fd0fa44b278667715f2b2eb7
AMT98 <aswinmalla12@gmail.com>
2023-01-05 20:14:18 -0700

    test3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
-app.use(morgan('dev'));
+app.use(morgan( 'dev' ));  
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
