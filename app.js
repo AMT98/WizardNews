1d02a234d1fbb6c8922aa4b13e4facdf718e937f
Vincent Palomo <vincentpalomo@yahoo.com>
2023-01-05 22:48:21 -0600

    added middleware and rendered html

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const path = require('path');
+const postBank = require('./postBank');
 
-app.use(morgan( 'dev' ));  
+app.use(morgan('dev'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const homeMiddleware = express.static(path.join(__dirname, 'public'));
+app.use(homeMiddleware);
+
+app.get('/', (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        )
+        .join('')}
+    </div>
+  </body>
+</html>`;
+
+  res.send(html);
+});
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
+  console.log(`App listening in port http://localhost:${PORT}/`);
 });
--- wnews/app.js
+++ wnews/app.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const postBank = require('./postBank');
 
-app.use(morgan( 'dev' ));  
+app.use(morgan('dev'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+  <title>Wizard News</title>
+  </head>
+  <body>
+  <ul>
+  ${posts.map((post) => `<li>${post.content}</li>`)}
+  </ul>
+  </body>
+  </html>`;
+});
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
+  console.log(`App listening in port http://localhost:${PORT}`);
 });
-
