34bec176cb51c4ac71ba98e3b41fe097f9f07ee8
AMT98 <aswinmalla12@gmail.com>
2023-01-08 22:05:29 -0700

    add GET /:id route

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.use(homeMiddleware);
 
 app.get('/', (req, res) => {
   const posts = postBank.list();
-
   const html = `<!DOCTYPE html>
   <html>
   <head>
@@ -27,7 +26,7 @@ app.get('/', (req, res) => {
         <div class='news-item'>
           <p>
             <span class="news-position">${post.id}. ▲</span>
-            ${post.title}
+            <a href="/posts/${post.id}">${post.title}</a>
             <small>(by ${post.name})</small>
           </p>
           <small class="news-info">
@@ -42,6 +41,38 @@ app.get('/', (req, res) => {
 
   res.send(html);
 });
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+            
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>
+    </div>
+  </body>
+</html>`;
+
+  res.send(html);
+});
+
 
 const PORT = 1337;
 
