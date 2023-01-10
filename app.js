d788e06680a22a0803e36e337a2d89880f196bdd
Vincent Palomo <vincentpalomo@yahoo.com>
2023-01-09 21:04:30 -0600

    finished views and is working on live server

--- app.js
+++ app.js
@@ -3,98 +3,31 @@ const app = express();
 const morgan = require('morgan');
 const path = require('path');
 const postBank = require('./postBank');
+const postList = require('./views/postList');
+const postDetail = require('./views/postDetails');
 
 app.use(morgan('dev'));
 
 const homeMiddleware = express.static(path.join(__dirname, 'public'));
 app.use(homeMiddleware);
 
+// list
 app.get('/', (req, res) => {
-  const posts = postBank.list();
-  const html = `<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Wizard News</title>
-    <link rel="stylesheet" href="/style.css" />
-  </head>
-  <body>
-    <div class="news-list">
-      <header><img src="/logo.png"/>Wizard News</header>
-      ${posts
-        .map(
-          (post) => `
-        <div class='news-item'>
-          <p>
-            <span class="news-position">${post.id}. ▲</span>
-            <a href="/posts/${post.id}">${post.title}</a>
-            <small>(by ${post.name})</small>
-          </p>
-          <small class="news-info">
-            ${post.upvotes} upvotes | ${post.date}
-          </small>
-        </div>`
-        )
-        .join('')}
-    </div>
-  </body>
-</html>`;
-
-  res.send(html);
+  res.send(postList);
 });
 
 app.get('/posts/:id', (req, res) => {
   const id = req.params.id;
   const post = postBank.find(id);
   if (!post.id) {
-    res.status(404)
-    const html = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Wizard News</title>
-      <link rel="stylesheet" href="/style.css" />
-    </head>
-    <body>
-      <header><img src="/logo.png"/>Wizard News</header>
-      <div class="not-found">
-        <p>Accio Page! 🧙‍♀️ ... Page Not Found</p>
-        <img src="/dumbledore-404.gif" />
-      </div>
-    </body>
-    </html>`
-    res.send(html)
+    res.status(404);
+    res.send(postDetail.error);
   } else {
-  const html = `<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Wizard News</title>
-    <link rel="stylesheet" href="/style.css" />
-  </head>
-  <body>
-    <div class="news-list">
-      <header><img src="/logo.png"/>Wizard News</header>
-      
-        <div class='news-item'>
-          <p>
-            <span class="news-position">${post.id}. ▲</span>
-            <a href="/posts/${post.id}">${post.title}</a>
-            <small>(by ${post.name})</small>
-            <p>${post.content}</p>
-          </p>
-          <small class="news-info">
-            ${post.upvotes} upvotes | ${post.date}
-          </small>
-        </div>
-    </div>
-  </body>
-</html>`;
-
-  res.send(html);}
+    res.send(postDetail.detail(post));
+  }
 });
 
-
 const { PORT = 1337 } = process.env;
-
 
 app.listen(PORT, () => {
   console.log(`App listening in port http://localhost:${PORT}/`);
---
+++ views/postDetails.js
@@ -0,0 +1,44 @@
+const postBank = require('../postBank');
+
+const error = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class="not-found">
+        <p>Accio Page! 🧙‍♀️ ... Page Not Found</p>
+        <img src="/dumbledore-404.gif" />
+      </div>
+    </body>
+    </html>`;
+
+const detail = (post) => `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+            <p>${post.content}</p>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>
+    </div>
+  </body>
+</html>`;
+
+module.exports = { error: error, detail: detail };
